fcab828765c2876def713b59ae72afd1415527aa
c w <charlesw.yc.hs.@gmail.com>
2026-07-18 22:13:54 -0400

    userandpassword.py

--- SidebarPage.py
+++ SidebarPage.py
@@ -1,11 +1,43 @@
 import streamlit as st
 
-st.logo("https://tse3.mm.bing.net/th/id/OIP.PRn2vZX_fiQ9yOq7y8G8RgHaEG?r=0&rs=1&pid=ImgDetMain&o=7&rm=3"), size='large', link="https://cwblogpost.streamlit.app/"
+st.logo("https://tse3.mm.bing.net/th/id/OIP.PRn2vZX_fiQ9yOq7y8G8RgHaEG?r=0&rs=1&pid=ImgDetMain&o=7&rm=3", size='large', link="https://cwblogpost.streamlit.app/")
+
+if "logged_in" not in st.session_state:
+    st.session_state.logged_in = False
+
+def login():
+    st.title("Log in")
+    with st.form("Login_form"):
+        username = st.text_input("Username")
+        password = st.text_input("Password", type="password")
+        submitted = st.form_submit_button("Log in")
+
+        if submitted:
+            if username =='Ballsackz' and password == 'Garyisgay':
+                st.session_state.logged_in = True
+                st.session_state.username = username
+                st.rerun()
+            else:
+                st.error("Incorrect username OR password")
+
+def logout():
+    st.session_state.logged_in = False
+    st.rerun()
+
+login_page = st.Page(login, title="Log in", icon=":material/login:")
+logout_page = st.Page(logout, title="Log out", icon=":material/logout:")
 
 pages = {
     "Daily Life": [
-        st.page("main.py", tittle="Self Intro")
-
-    ]
-
+        st.Page("main.py", title="Self Intro"),
+        st.Page("food.py", title = "Food", icon=":material/icecream:"),
+    ],
+"Settings": [logout_page]
 }
+
+if st.session_state.logged_in:
+    pg = st.navigation(pages)
+else: 
+    pg = st.navigation([login_page])
+
+pg.run()
---
+++ food.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("This is my food page!")
+
+st.header(" My favourite food is cheese")
+st.subheader("🧀🧀🧀🧀🧀🧀🧀🧀🧀🧀🧀🧀🧀🧀🧀🧀")
+
+st.markdown("""
+  I like cheese because it tastes good and is also decently good for you!
+    I also like cheese because it has a good texture as it is squishy yet solid.
+""")
+
+st.video("https://www.youtube.com/watch?v=G51iyxgLodc&list=RDG51iyxgLodc&start_radio=1")
