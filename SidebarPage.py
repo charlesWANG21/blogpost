1a1c830ae1044762250a8e109fe248c012700223
c w <charlesw.yc.hs.@gmail.com>
2026-07-18 21:38:45 -0400

    main.py

---
+++ SidebarPage.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.logo("https://tse3.mm.bing.net/th/id/OIP.PRn2vZX_fiQ9yOq7y8G8RgHaEG?r=0&rs=1&pid=ImgDetMain&o=7&rm=3"), size='large', link="https://cwblogpost.streamlit.app/"
+
+pages = {
+    "Daily Life": [
+        st.page("main.py", tittle="Self Intro")
+
+    ]
+
+}
